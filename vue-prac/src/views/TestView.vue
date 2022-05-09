570b1c1ccc706d3b55805758ea9cc9ed18daa95f
Kyuuu827 <lck0827@gmail.com>
2022-05-09 22:55:59 +0900

    Test

--- vue-prac/src/views/TestView.vue
+++ vue-prac/src/views/TestView.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="home">
-    <HelloWorld msg="이것은 테스트다"/>
+    <HelloWorld msg="Vue.js Practice"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default  {
