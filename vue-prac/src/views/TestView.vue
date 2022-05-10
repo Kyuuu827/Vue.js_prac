c09d580a1057e8a3473adb841f42cfca521fc121
Kyuuu827 <lck0827@gmail.com>
2022-05-10 23:14:30 +0900

    TestView 컴포넌트 수정

--- vue-prac/src/views/TestView.vue
+++ vue-prac/src/views/TestView.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <HelloWorld msg="Vue.js Practice"/>
+    <home-view />
   </div>
 </template>
 
 <script>
 import HelloWorld from '@/components/HelloWorld.vue'
+import HomeView from './HomeView.vue'
 
 export default  {
   name: 'TestView',
   components: {
-    HelloWorld
+    HelloWorld,
+    HomeView
   },
   setup() {
 
